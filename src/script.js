9b5add68c663249c514d52df5c299f725a7d3c8a
axelquartz <axelarep@gmail.com>
2025-04-21 15:04:05 -0600

    Responsive fixes to test

--- src/script.js
+++ src/script.js
@@ -1,8 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
     const burgerMenuButton = document.querySelector('.burger-menu-bar label');
     const burgerMenu = document.querySelector('.burger-menu');
+    const burgerIcon = document.querySelector('.burger-icon');
 
     burgerMenuButton.addEventListener('click', function() {
         burgerMenu.classList.toggle('active');
+        burgerIcon.classList.toggle('active');
+    });
+
+    // Close menu when clicking outside
+    document.addEventListener('click', function(event) {
+        const isClickInside = burgerMenuButton.contains(event.target) || burgerMenu.contains(event.target);
+        
+        if (!isClickInside && burgerMenu.classList.contains('active')) {
+            burgerMenu.classList.remove('active');
+            burgerIcon.classList.remove('active');
+        }
     });
 });
